013a780e19ae423db83528d0872cfd111b08299a
Muhammad Zaryab <zaryabkhattak778800@gmail.com>
2023-09-04 11:20:42 +0500

    set feed,style

--- assets/js/Headerfooter.js
+++ assets/js/Headerfooter.js
@@ -1,17 +1,15 @@
- 
-    // Include header.html
-    const headerContainer = document.getElementById("headerContainer");
-    fetch("header.html")
-        .then(response => response.text())
-        .then(content => {
-            headerContainer.innerHTML = content;
-        });
+// Include header.html
+const headerContainer = document.getElementById("headerContainer");
+fetch("header.html")
+  .then((response) => response.text())
+  .then((content) => {
+    headerContainer.innerHTML = content;
+  });
 
-    // Include footer.html
-    const footerContainer = document.getElementById("footerContainer");
-    fetch("footer.html")
-        .then(response => response.text())
-        .then(content => {
-            footerContainer.innerHTML = content;
-        });
- 
+// Include footer.html
+const footerContainer = document.getElementById("footerContainer");
+fetch("footer.html")
+  .then((response) => response.text())
+  .then((content) => {
+    footerContainer.innerHTML = content;
+  });
